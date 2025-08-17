93e868dee74f6774931ab37d1a8a38b9d6cdbfda
Jeff Strinko <jstrinko@gmail.com>
2025-08-17 00:39:56 -0400

    make it work in mobile

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -4,9 +4,11 @@ import SignupForm from './Home/SignupForm';
 export default function Events() {
   return (
     <PageLayout>
-      <section className="mb-8 min-w-2xl">
-        <div className="w-full flex justify-center">
-          <SignupForm />
+      <section className="mb-8">
+        <div className="w-full flex justify-center px-4">
+          <div className="w-full sm:w-auto max-w-3xl">
+            <SignupForm />
+          </div>
         </div>
       </section>
     </PageLayout>
