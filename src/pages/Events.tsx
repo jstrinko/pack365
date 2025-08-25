63075f1d71e8b6483961087615840e36b93ccc55
Jeff Strinko <jstrinko@gmail.com>
2025-08-24 22:24:10 -0400

    beginnings of a slideshow

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -2,13 +2,20 @@
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import PageLayout from "../components/PageLayout";
 import EventCalendar from './Events/EventCalendar';
+import EventSlideShow from './Events/EventSlideShow';
+import { eventList } from './Events/Data';
 
 export default function Events() {
   return (
     <PageLayout>
       <h2 className="text-2xl font-bold mb-4">Events</h2>
-      <p>Here are the exciting campouts, meetups, and ceremonies scheduled for this scouting year! Join us for a ton of fun!</p>
-      <EventCalendar />
+      <section className="mb-8">
+        <p>Here are the exciting campouts, meetups, and ceremonies scheduled for this scouting year! Join us for a ton of fun!</p>
+        <EventCalendar />
+      </section>
+      <section className="mb-8">
+        <EventSlideShow events={eventList} />
+      </section>
     </PageLayout>
   );
 }
---
+++ src/pages/Events/Data.tsx
@@ -0,0 +1,22 @@
+const eventList = [
+  {
+    name: "Scouts Swimming & BBQ",
+    description: "We had a wonderful time reconnecting with old friends and meeting new ones at the scouts swimming & BBQ event.",
+    date: new Date("2025-08-19"),
+    photos: [
+      { path: "/slideshow/IMG_0174.jpg", name: "Pool Area", description: "Kicking off the school year" },
+      { path: "/slideshow/IMG_0176.jpg", name: "Enjoying Food and Friends", description: "Great turnout for the swimming event!" },
+    ],
+  },
+  {
+    name: "Onboarding",
+    description: "Onboarding new scouts at Waters Edge Elementary",
+    date: new Date("2025-08-21"),
+    photos: [
+      { path: "/slideshow/IMG_0179.jpg", name: "Onboarding Photo 1", description: "Getting to know each other" },
+      { path: "/slideshow/IMG_0181.jpg", name: "Onboarding Photo 2", description: "Team building activities" },
+    ],
+  },
+];
+
+export { eventList };
