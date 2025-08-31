72c1564f759c1bac2157cc0e60624029b258a88b
Jeff Strinko <jstrinko@gmail.com>
2025-08-31 15:16:29 -0400

    add new events, add facebook link, add calendar link

--- src/pages/Events/Data.tsx
+++ src/pages/Events/Data.tsx
@@ -9,7 +9,7 @@ const eventList = [
     ],
   },
   {
-    name: "Onboarding",
+    name: "Onboarding @ Waters Edge",
     description: "Onboarding new scouts at Waters Edge Elementary",
     date: new Date("2025-08-21"),
     photos: [
@@ -17,6 +17,26 @@ const eventList = [
       { path: "/slideshow/IMG_0181.jpg", name: "Onboarding Photo 2", description: "Team building activities" },
     ],
   },
+  {
+    name: "Onboarding @ Sandpiper Shores",
+    description: "Onboarding new scouts at Sandpiper Shores Elementary",
+    date: new Date("2025-08-28"),
+    photos: [
+      { path: "/slideshow/IMG_0183.jpg", name: "Onboarding Photo 1", description: "Getting to know each other" },
+      { path: "/slideshow/IMG_0186.jpg", name: "Onboarding Photo 2", description: "Team building activities" },
+      { path: "/slideshow/IMG_0187.jpg", name: "Onboarding Photo 3", description: "Team building activities" },
+      { path: "/slideshow/IMG_0184.jpg", name: "Onboarding Photo 4", description: "Team building activities" },
+    ],
+  },
+  {
+    name: "Popcorn Kickoff",
+    description: "Kickoff event for popcorn sales",
+    date: new Date("2025-08-30"),
+    photos: [
+      { path: "/slideshow/IMG_0190.jpg", name: "Popcorn Kickoff Photo 1", description: "Excited scout ready to sell popcorn" },
+      { path: "/slideshow/IMG_0192.jpg", name: "Popcorn Kickoff Photo 2", description: "Gaining community support" },
+    ],
+  }
 ];
 
 export { eventList };
